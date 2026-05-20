a75111ad5ff8c8c29e1295d4966ccb98e847534a
Bruno <manobrunodutra@gmail.com>
2026-05-20 19:25:57 -0300

    Fix tailscale device

--- modules/notebook.nix
+++ modules/notebook.nix
@@ -3,17 +3,21 @@
 {
     networking.hostName = "notebook";
 
+    # --- Bluetooth ---
     hardware.bluetooth = {
         enable = true;
         powerOnBoot = true;
     };
     services.blueman.enable = true;
+
     services.upower.enable = true;
     services.power-profiles-daemon.enable = true;
 
+    # --- Docker ---
     virtualisation.docker.enable = true;
-
     users.users.bruno.extraGroups = [ "docker" ];
+
+    services.tailscale.enable = true;
 
     users.users.bruno.packages = with pkgs; [
         # --- Api ---
