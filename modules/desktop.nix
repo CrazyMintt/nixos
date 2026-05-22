f51fa757e2b475033d7bf829b8a77a9197cfeb3a
Bruno <manobrunodutra@gmail.com>
2026-05-22 02:32:45 -0300

    Nvidia drivers

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,8 +1,19 @@
-{ pkgs, ... }:
+{ config, ... }:
 
 {
     networking.hostName = "desktop";
 
-    users.users.bruno.packages = with pkgs; [
-    ];
+    hardware.nvidia = {
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    
+    services.xserver.videoDrivers = [ "nvidia" ];
 }
